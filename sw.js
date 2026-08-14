3a57507fb337677a809ce4569657c43c2bba98bb
Brishank <brishanksingh7843@gmail.com>
2026-08-14 18:10:46 +0530

    Add additional files to service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
 const cn = "movie-app-v1";
-const ftc = ["./index.html", "./main.css", "./main.js", "./"];
+const ftc = [
+  "./index.html",
+  "./main.css",
+  "./main.js",
+  "./backwork.js",
+  "./",
+  "./account.html",
+  "./upload.html",
+  "./series.html",
+  "./signuppg.html",
+  "./movies.html",
+  "./latest.html",
+];
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cn).then((cache) => {
